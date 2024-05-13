d4fd1bc4b118270cfbe88ddec5ef52b1f1b82f99
camilo netbook <7camiloherrera@gmail.com>
2024-05-13 09:56:54 -0300

    EJE7 completo

--- Ejercicios no obligatorios/Practica 2/EJE7/B/EJE7B.go
+++ Ejercicios no obligatorios/Practica 2/EJE7/B/EJE7B.go
@@ -7,30 +7,54 @@ import (
 	"unicode"
 )
 func main()  {
-	var letrasMayus,letrasMinus,numeros,especiales int
+	var letrasMayus,letrasMinus,especiales int
+	var numerosMap = map[int]int{
+		0: 0,
+		1: 0,
+		2: 0,
+		3: 0,
+		4: 0,
+		5: 0,
+		6: 0,
+		7: 0,
+		8: 0,
+		9: 0,
+	}
 	read := bufio.NewReader(os.Stdin)
 
 	fmt.Println("ingrese la secuencia de caracteres")
 	input, _ := read.ReadString('\r')
 
 	secuencia:= []rune(input[:len(input)-1])
-	filtro(secuencia,&letrasMayus,&letrasMinus,&numeros,&especiales)
+	filtro(secuencia,&letrasMayus,&letrasMinus,&especiales,numerosMap)
+	fmt.Println("mayusculas")
 	fmt.Println(letrasMayus)
+	fmt.Println("minusculas")
 	fmt.Println(letrasMinus)
-	fmt.Println(numeros)
+	fmt.Println("map de numeros")
+	fmt.Println(numerosMap)
+	fmt.Println("especiales")
 	fmt.Println(especiales)
 }
 
-func filtro(s []rune,lMayu,lMin,n,e *int)  {
-	for _,r :=range s{
-		if(unicode.IsUpper(r)){
-			*lMayu++
-		}else if(unicode.IsLower(r)){
-			*lMin++
-		}else if(unicode.IsNumber(r)){
-			*n++
-		}else {
-			*e++
+
+	func filtro(s []rune,lMayu,lMin,e *int, numerosMap map[int]int)  {
+		for _,r :=range s{
+			num := int(r - '0')
+			if(unicode.IsUpper(r)){
+				*lMayu++
+			}else if(unicode.IsLower(r)){
+				*lMin++
+			}else if(unicode.IsNumber(r)){
+				numerosMap[num]++
+			}else {
+				*e++
+			}
 		}
-	}
 }
+
+/*
+Cómo funciona el num:= int(r-'0') 
+'0' tiene un valor ASCII de 48 entonces cuando recibe el ASCII del numero 5 por ejemplo
+que tiene un vlaor de 53 hace 53 (del cinco) - 48 (del cero) = 5 entonces ya me da el 5 para buscarlo en el map
+*/
