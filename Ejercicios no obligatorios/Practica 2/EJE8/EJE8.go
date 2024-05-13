c170d4999793decfbe025b1c6b2859d627c23970
camilo netbook <7camiloherrera@gmail.com>
2024-05-13 10:44:39 -0300

    eje 8 a completar

---
+++ Ejercicios no obligatorios/Practica 2/EJE8/EJE8.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "strconv"
+)
+
+func main() {
+    
+}
+
+func Convert(v int, b int) string {
+    srtNum := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" // como valen 1 byte puedo acceder como un arreglo
+}
